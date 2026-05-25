7bef4e3e88ada88f3af9880328a5f549f2aabc01
NguyenMinhThuc-IT <nmthuc2007@gmail.com>
2026-05-25 08:30:32 +0700

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,30 @@
+print("---- HỆ THỐNG TIẾP NHẬN BỆNH ÁN ----")
+
+# Nhập dữ liệu từ lễ tân
+name_patient = input("Nhập họ và tên bệnh nhân: ")
+medical_code = input("Nhập mã bệnh án (VD: BN1024): ")
+department = input("Nhập khoa/phòng khám chỉ định: ")
+
+# Hiển thị phiếu khám bệnh điện tử
+print("\n---- PHIẾU KHÁM BỆNH ĐIỆN TỬ ----")
+print(f"Bệnh nhân: {name_patient} – Mã BA: {medical_code} – Chuyển tới: {department}")
+
+# Input (dữ liệu đầu vào):
+# Họ và tên bệnh nhân → kiểu str
+# Mã bệnh án (ví dụ: BN1024) → kiểu str
+# Khoa/Phòng khám chỉ định → kiểu str
+
+# Hiển thị tiêu đề hệ thống.
+# Nhập dữ liệu từ bàn phím (CLI).
+# Lưu dữ liệu vào biến tương ứng.
+# In ra phiếu khám bệnh điện tử với định dạng chuẩn.
+
+# BEGIN
+#   PRINT "HỆ THỐNG TIẾP NHẬN BỆNH ÁN"
+#   INPUT name_patient
+#   INPUT medical_code
+#   INPUT department
+#   PRINT "PHIẾU KHÁM BỆNH ĐIỆN TỬ"
+#   PRINT "Bệnh nhân: [name_patient] – Mã BA: [medical_code] – Chuyển tới: [department]"
+# END
+
